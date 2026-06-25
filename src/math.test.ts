08b044b15e022e624def7a1975145b2f0f787820
Somasundaram Ayyappan <1802828+somus@users.noreply.github.com>
2026-06-25 12:53:18 +0530

    test: add multiply helper

--- src/math.test.ts
+++ src/math.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "bun:test";
-import { add, divide } from "./math";
+import { add, divide, multiply } from "./math";
 
 test("add returns the sum", () => {
   expect(add(2, 3)).toBe(5);
@@ -7,4 +7,8 @@ test("add returns the sum", () => {
 
 test("divide returns the quotient", () => {
   expect(divide(8, 2)).toBe(4);
+});
+
+test("multiply returns the product", () => {
+  expect(multiply(4, 5)).toBe(20);
 });
